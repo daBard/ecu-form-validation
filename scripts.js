7120663678c68092459bb55d58445f33c328a4b0
daBard <50872455+daBard@users.noreply.github.com>
2023-10-18 07:10:40 +0200

    created repo

---
+++ scripts.js
@@ -0,0 +1,104 @@
+const errorMessages = {
+    fname: 'Must be at least one character',
+    lname: 'Must be at least one character',
+    email: 'Must be a valid email address',
+    phone: 'Must be a valid swedish cell phone number',
+    pin: 'Must be exactly six numerical digits.',
+    pinConfirm: 'Pin confirmation must be the same as the pin'
+}
+
+const defaultMessages = {
+    fname: '',
+    lname: '',
+    email: '',
+    phone: '',
+    pin: 'The pin needs to be exactly six numerical digits long.',
+    pinConfirm: '',
+}
+
+function validateKeyUp (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(`#${e.target.id}-object`);
+    //const errorP = document.querySelector(`#${target.id} > p`);
+    target.classList.remove('error');
+
+}
+
+function validateSubmit(e) {
+    e.preventDefault();
+    
+    const user = {
+        fname: document.querySelector('#fname'),
+        lname: document.querySelector('#lname'),
+        email: document.querySelector('#email'),
+        phone: document.querySelector('#phone'),
+        pin: document.querySelector('#pin'),
+        pinConfirm: document.querySelector('#pinConfirm')
+    }
+
+    for (const key in user ) {
+        ifEmpty(user[key]);
+        validationSwitch(user[key]);
+    }   
+}
+
+function ifEmpty(obj) {
+
+    console.log(obj.value);
+
+    if (obj.value == "") {
+        error(obj, true)
+    }
+    else {
+        error(obj, false)
+    }
+}
+
+function validationSwitch(obj) {
+    switch(obj.type) {
+        case 'text':
+            console.log('text switch')
+            break;
+        default:
+            break;
+    }
+    
+    switch(obj.id) {
+        case 'email':
+            console.log('email switch')
+            break;
+        case 'phone':
+            console.log('phone switch')
+            const phoneRegex = /^(?:\+46|0046)(7[0236]\d{6})$/;
+            error(obj, !phoneRegex.test(obj.value));
+            break;
+        case 'pin':
+            console.log('pin switch')
+            const pinRegex = /^\d{6}$/;
+            error(obj, !pinRegex.test(obj.value));
+            break;
+        case 'pinConfirm':
+            if (pinConfirm) {
+                console.log('pinConfirm switch')
+                break;}
+            else
+                break;
+        default:
+          break;
+      }
+}
+
+function error(obj, add) {
+    const target = document.querySelector(`#${obj.id}-object`);
+    const errorP = document.querySelector(`#${target.id} > p`);
+
+    if (add) {
+        target.classList.add('error');
+        errorP.innerHTML = errorMessages[obj.id];
+    }
+    else {
+        target.classList.remove('error');
+        errorP.innerHTML = defaultMessages[obj.id];
+    }
+}
